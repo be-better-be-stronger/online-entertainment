d4f006d16558b02ebd83a975e8d451a2082e6e68
be-better-be-stronger <dangquocthanhsaclo@gmail.com>
2026-04-08 21:23:19 +0700

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
+module.exports = { 
+  extends: ['@commitlint/config-conventional'], 
+  rules: { 
+    'scope-enum': [2, 'always', [ 'auth', 'user', 'video', 'admin', 'dao', 'service' ]] 
+  } 
 };
