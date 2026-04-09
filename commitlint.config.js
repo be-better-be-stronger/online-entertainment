3453a784cd523a9cb1700e64fe2dabdd101cb5f3
be-better-be-stronger <dangquocthanhsaclo@gmail.com>
2026-04-09 14:54:17 +0700

    chore(config): add cleanup scope to commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = { 
   extends: ['@commitlint/config-conventional'], 
   rules: { 
-    'scope-enum': [2, 'always', [ 'auth', 'user', 'video', 'admin', 'dao', 'favorite', 'service', 'config' ]] 
+    'scope-enum': [2, 'always', [ 'auth', 'user', 'video', 'admin', 'dao', 'favorite', 'service', 'config', 'cleanup' ]] 
   } 
 };
